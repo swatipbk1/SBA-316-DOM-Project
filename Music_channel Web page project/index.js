bccfd32ec5b833fc29e8ef101d87b2ddd6f17a3f
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-02 18:15:50 -0800

    c

--- Music_channel Web page project/index.js
+++ Music_channel Web page project/index.js
@@ -4,32 +4,39 @@ console.log("Welcome to Angelic Music");
 
 let songIndex=0;
 let audioElement= new Audio('1.mp3');
-let masterPlay= document.getElementById("masterPlay");
-let myProgressBar=document.getElementById("myProgressBar");
+let masterPlay= document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
 let songs= [
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"}
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautifuljourney", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
 ]
 
-audioElement.play();
+
 
 
 
 //Handle Play/Pause Click
-masterPlay.addEventListener("click", () => {
+masterPlay.addEventListener("click",() =>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
     }
-
+    else {
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
 })
 
 
+
+
 //Listen to Events
-myProgressBar.addEventListener( "timeupdate", () => {
-    console.log("timeupdate");
+myProgressBar.addEventListener('timeupdate', () => {
+    console.log('timeupdate');
+    //Update seekbar
 });
