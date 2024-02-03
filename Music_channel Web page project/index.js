a7f144b61cfecc6c11acfb0e81f246a251126390
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-02 17:26:31 -0800

    s

--- Music_channel Web page project/index.js
+++ Music_channel Web page project/index.js
@@ -1,3 +1,14 @@
 console.log("Welcome to Angelic Music");
+
+//Intialize
+let songs= [
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"beautiful journey", filepath:"song/1.mp3", coverPath:"covers/1.jpg"}
+]
 let audioElement= new Audio('1.mp3');
 audioElement.play();
